679ce484ab44787ef439ad66f3880e5fe1a42ed5
NuGelaLies <nugelalies@gmail.com>
2020-12-17 13:41:44 +0800

    delete cache file and add gitignore

--- Pods/Target Support Files/Pods-TMDBSwiftUI/Pods-TMDBSwiftUI-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TMDBSwiftUI : NSObject
-@end
-@implementation PodsDummy_Pods_TMDBSwiftUI
-@end
--- TMDBSwiftUI/Utils/Network/AnalysisJSON/AnalysisJSON.swift
+++
@@ -1,7 +0,0 @@
-//
-//  AnalysisJSON.swift
-//  HeadlineNews
-//
-//  Created by NuGelaLies on 2019/7/2.
-//  Copyright © 2019 Sing Tao Limited. All rights reserved.
-//
